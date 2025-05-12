acf5879e29794de0257a1566be068568b3be46e5
mufleha19 <muflehamubashir@gmail.com>
2025-05-12 23:38:38 +0500

    fix: Firebase config with env vars

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeP6FwogRse7t_1Fws_n2YU5EEgmWqvYQ",
-  authDomain: "expense-tracker-e6541.firebaseapp.com",
-  databaseURL: "https://expense-tracker-e6541-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "expense-tracker-e6541",
-  storageBucket: "expense-tracker-e6541.firebasestorage.app",
-  messagingSenderId: "987305771954",
-  appId: "1:987305771954:web:bcd6ddd1265f5156c85aab"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
